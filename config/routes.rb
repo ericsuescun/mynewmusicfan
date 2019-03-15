6aba04f46997c93021b0aeeb938b312552f3e084
Eric Suescun <edsuescun@gmail.com>
2019-03-15 17:01:58 -0500

    Agregados las referencias a bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+	get '/songs', to: 'song#index'
+
+	resources :artists do
+		resources :album do
+			resources :song, only: [:show]
+		end
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
